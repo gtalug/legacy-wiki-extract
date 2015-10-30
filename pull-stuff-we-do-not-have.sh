04ca5539f4c0ef45d0d2155728f2ee097f6a84f5
Christopher Browne <cbbrowne@ca.afilias.info>
2015-10-30 16:53:23 -0400

    Bug, wrong name for  new page

--- pull-stuff-we-do-not-have.sh
+++ pull-stuff-we-do-not-have.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 TARGET=legacy-pages
 
+# Revise set of pages based on current state
+(echo "Main_page"; cat ${TARGET}/*.html | ./scrape-pages-from-wiki.sh | sort | uniq) > all_pages
+
 for page in `cat all_pages | sort | uniq`; do
     if [ -e ${TARGET}/${page}.html ]; then
 	echo "already got ${TARGET}/${page}"
     else
-	wget -O ${TARGET}/{page}.html "https://web.archive.org/web/20100204092254/http://tlug.ss.org/wiki/${page}"
+	wget -O ${TARGET}/${page}.html "https://web.archive.org/web/20100204092254/http://tlug.ss.org/wiki/${page}"
     fi
 done
 
-## And revise the set of available pages
-(echo "Main_page"; cat ${TARGET}/*.html | ./scrape-pages-from-wiki.sh | sort | uniq) > all_pages
+
