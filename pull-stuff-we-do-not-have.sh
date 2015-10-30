55fe895136dad0c20ed8dac4ea2a0556be4a1412
Christopher Browne <cbbrowne@ca.afilias.info>
2015-10-30 16:49:39 -0400

    Shift pages into subdirectory

--- pull-stuff-we-do-not-have.sh
+++ pull-stuff-we-do-not-have.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-echo "Main_page" > all_pages
+TARGET=legacy-pages
 
 for page in `cat all_pages | sort | uniq`; do
-    if [ -e ${page}.html ]; then
-	echo "already got ${page}"
+    if [ -e ${TARGET}/${page}.html ]; then
+	echo "already got ${TARGET}/${page}"
     else
-	wget -O ${page}.html "https://web.archive.org/web/20100204092254/http://tlug.ss.org/wiki/${page}"
+	wget -O ${TARGET}/{page}.html "https://web.archive.org/web/20100204092254/http://tlug.ss.org/wiki/${page}"
     fi
 done
 
 ## And revise the set of available pages
-cat *.html | ./scrape-pages-from-wiki.sh > all_pages
+(echo "Main_page"; cat ${TARGET}/*.html | ./scrape-pages-from-wiki.sh | sort | uniq) > all_pages
